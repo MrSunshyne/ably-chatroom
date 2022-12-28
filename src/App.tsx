29ecc513b1f5c2ccf8e0cb782d5c5d48de7b0d4f
Sandeep Ramgolam <sandeep@ramgolam.com>
2022-12-28 18:19:32 +0400

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { AblyConnect } from './components/ably-connect'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { AblyConnect } from "./components/ably-connect";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <AblyConnect/>
+    <div className="App h-screen p-8">
+      <div className="bg-gray-100 p-8 h-full">
+        <AblyConnect />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
